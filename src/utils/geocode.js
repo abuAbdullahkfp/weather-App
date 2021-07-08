cdbc32525382019f62127a2fa6fa90677b8d41e2
afrid <afrid37@gmail.com>
2021-07-08 22:27:18 +0530

    bug fixed

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,6 +2,7 @@ const request = require('request')
 
 const geocode = (address, callback) => {
    const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiYWZyaWRhYnVhYmR1bGxhaCIsImEiOiJja3F0MGxwc2YxZ2M5MzFxYTNkN2I0cGdqIn0.TAOTl3QJg1Fl_7SvOtHZeQ&limit=1`;
+   console.log(geoUrl)
    request({url:geoUrl, json:true}, (error, response)=> {
       if (error){
         callback('Unable to connect to the Internet', undefined)
@@ -9,18 +10,18 @@ const geocode = (address, callback) => {
         callback('Cant find the location', undefined)
       }else {
         callback(undefined, {
-          latitude: response.body.features[0].center[0],
+          latitute: response.body.features[0].center[0],
           longitude: response.body.features[0].center[1],
           placeName: response.body.features[0].place_name
         })
-        debugger
       }
    })
 }
 
 const foreCast = (location, callback) => {
-    const {latitude, longitude} = location
-    const url = `http://api.weatherstack.com/current?access_key=357bb94d77b774308284f5ae9d4176a0&query=${longitude},${latitude}`;
+    const {latitute, longitude} = location
+    const url = `http://api.weatherstack.com/current?access_key=357bb94d77b774308284f5ae9d4176a0&query=${longitude},${latitute}`;
+    console.log(url)
     request({url, json:true}, (error, response)=> {
        if (error){
           callback('Unable to Connect to the internet', undefined)
